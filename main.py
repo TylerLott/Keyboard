acaf52e72e92b36d484751497c5aa84ac68b803b
TylerLott <tylerlott0126@gmail.com>
2023-04-09 12:09:36 -0600

    brought over old keymap

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ _______ = KC.TRNS
 XXXXXXX = KC.NO
 
 # layers
-SYMB = KC.MO(1)
-NUMB = KC.MO(2)
+NUMB = KC.MO(1)
+SYMB = KC.MO(2)
 NAV_M = KC.MO(3)
 NAV_W = KC.MO(4)
 FUNC = KC.MO(5)
@@ -52,12 +52,12 @@ PWRD = send_string(PASSWORD)
 UPS = simple_key_sequence(USER_PASS_SEQ)
 
 # Homerow modtap
-F_GUI = KC.MT(KC.F, KC.LGUI)
-D_ALT = KC.MT(KC.D, KC.LALT)
-S_CTL = KC.MT(KC.S, KC.LCTRL)
-J_GUI = KC.MT(KC.J, KC.RGUI)
-K_ALT = KC.MT(KC.K, KC.RALT)
-L_CTL = KC.MT(KC.L, KC.RCTRL)
+F_GUI = KC.MT(KC.F, KC.LGUI, tap_interrupted=False, prefer_hold=False)
+D_ALT = KC.MT(KC.D, KC.LALT, tap_interrupted=False, prefer_hold=False)
+S_CTL = KC.MT(KC.S, KC.LCTRL, tap_interrupted=False, prefer_hold=False)
+J_GUI = KC.MT(KC.J, KC.RGUI, tap_interrupted=False, prefer_hold=False)
+K_ALT = KC.MT(KC.K, KC.RALT, tap_interrupted=False, prefer_hold=False)
+L_CTL = KC.MT(KC.L, KC.RCTRL, tap_interrupted=False, prefer_hold=False)
 
 ############## KEYMAP ########################
 
@@ -65,52 +65,52 @@ L_CTL = KC.MT(KC.L, KC.RCTRL)
 keyboard.keymap = [
     [  #QWERTY
         KC.ESC,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.GRV,\
-        KC.LCTL,   KC.A,   S_CTL,    D_ALT,  F_GUI,    KC.G,                         KC.H,   J_GUI,   K_ALT,   L_CTL, KC.SCLN, KC.ENT,\
+        KC.TAB,    KC.A,   S_CTL,    D_ALT,  F_GUI,    KC.G,                         KC.H,   J_GUI,   K_ALT,   L_CTL, KC.SCLN, KC.ENT,\
         KC.CW,     KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.BSLS,\
                                              NAV_M,    SYMB,  KC.LSFT,   KC.BSPC,  KC.SPC,    NUMB,
     ],
-    [  #SYMB
-        KC.ESC,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,     KC.N7,   KC.N8,   KC.N9,   KC.N0, _______,\
-        _______, _______, KC.LCTL, KC.LALT, KC.LGUI, _______,                        _______, KC.RGUI, KC.RALT,KC.RCTRL, _______, _______,\
-        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                               FUNC,    CUST,  _______,   _______,   _______, NUMB,
-    ],
     [  #NUM
-        KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                         KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,\
-        KC.LCTL, _______, _______, _______, _______, _______,                        KC.MINS,  KC.EQL, KC.LCBR, KC.RCBR, KC.PIPE,  KC.GRV,\
-        KC.LSFT, _______, _______, _______, _______, _______,                        KC.UNDS, KC.PLUS, KC.LBRC, KC.RBRC, KC.BSLS, KC.TILD,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+        _______,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,     KC.N7,   KC.N8,   KC.N9,   KC.N0, _______,\
+        _______, _______, KC.LCTL, KC.LALT, KC.LGUI, _______,                        _______, KC.RGUI, KC.RALT,KC.RCTRL, _______, _______,\
+        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC.COMM, _______, _______,\
+                                               FUNC,    CUST,  KC.LSFT,   _______,   _______, NUMB,
+    ],
+    [  #SYMB
+        KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                         KC.MINS, KC.LCBR, KC.RCBR, _______, _______, KC.PIPE,\
+        KC.LCTL, _______, _______,KC.AMPR, KC.CIRC, KC.ASTR,                         KC.EQL,  KC.LPRN, KC.RPRN, KC.UNDS, KC.DQUO,  KC.QUOT,\
+        KC.LSFT, _______, _______, _______, _______, _______,                        KC.PLUS, KC.LBRC, KC.RBRC, _______, KC.QUES, _______,\
+                                            _______,   SYMB,  _______,     KC.DEL,   _______,    CUST,
     ],
     [  #NAV_M (mac navigation)
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+        _______,   KC.F1,    KC.F2,  KC.F3,   KC.F4,   KC.F5,                     KC.F6,     KC.F7,   KC.F8,    KC.F9,  KC.F10,  KC.F11,\
+        _______, KC.LSFT,  KC.LCTL,KC.LALT, KC.LGUI, _______,                     KC.LEFT, KC.DOWN,   KC.UP, KC.RIGHT, _______, _______,\
+        _______, _______, _______, _______, _______, _______,                     KC.HOME, KC.PGDN, KC.PGUP,   KC.END, _______, _______,\
+                                            NAV_M,   _______,  _______,  _______, _______,    CUST,
     ]
     ,
-    [  #NAV_W (windows navigation)
+    [  #NAV_W (windows navigation) TODO
         _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
         _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
         _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+                                              NAV_M, _______,  _______,  _______, _______,    CUST,
     ],
     [  #FUNC
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
+        _______,   KC.F1,    KC.F2,  KC.F3,   KC.F4,   KC.F5,                          KC.F6,   KC.F7,   KC.F8,   KC.F9,  KC.F10,  KC.F11,\
+        _______, KC.LSFT, KC.LCTL, KC.LALT, KC.LGUI, _______,                        _______, KC.RGUI, KC.RALT,KC.RCTRL, KC.RSFT, _______,\
         _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+                                               FUNC, _______,  _______,  _______,    _______,    FUNC,
     ],
     [  #CUST
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+        _______, _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
+        _______,    PASS,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
+        _______, _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
+                                              _______,    CUST,  _______,  _______,    _______,  CUST,
     ],
     [  #PASS
         _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-        _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
+        _______,  _______,  _______,     PWRD,     UPS, _______,                        _______, _______, _______, _______, _______, _______,\
         _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+                                              _______,    CUST,  _______,  _______,    _______,  CUST,
     ]
 ]
 
